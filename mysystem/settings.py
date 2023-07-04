60347afd3031474059189ff240937bb1489fc534
Rafafi123 <103036429+Rafafi123@users.noreply.github.com>
2023-07-05 07:57:58 +0800

    ststiasif

--- mysystem/settings.py
+++ mysystem/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_REDIRECT_URL = 'home' # Added 
 LOGOUT_REDIRECT_URL = 'login'
